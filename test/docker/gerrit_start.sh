7f2bed3121b2d43abdf1af9b618eb31b74cc65be
Nasser Grainawi <nasser@codeaurora.org>
2021-05-12 09:25:38 -0600

    fixup! test/docker: Use gerritcodereview docker image
    Remove now unused gerrit_start.sh script and the mount option to make it
    available to the gerrit container.
    
    Change-Id: I1789d2955297e030f40a9dcc21de9fd66d91e8ca

--- test/docker/gerrit_start.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Initializing Gerrit site ..."
-java -jar "$GERRIT_SITE"/bin/gerrit.war init --batch -d "$GERRIT_SITE"
-java -jar "$GERRIT_SITE"/bin/gerrit.war reindex -d "$GERRIT_SITE"
-echo "Running Gerrit ..."
-"$GERRIT_SITE"/bin/gerrit.sh run
