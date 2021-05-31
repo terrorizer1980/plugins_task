2c6387a00f629f277134ac18e9cbb794f63d7512
Adithya Chakilam <achakila@codeaurora.org>
2021-05-30 21:18:49 -0500

    UI: Indent sub-tasks
    Indent the sub-tasks, so that we can show a tree like structure in
    UI. This makes it eaiser to understand the on why tasks are waiting.
    
    Change-Id: I9bf11e609343b6e5caef0e112a07a7732e9b518e

--- gr-task-plugin/gr-task-plugin.js
+++ gr-task-plugin/gr-task-plugin.js
@@ -130,7 +130,7 @@
       return icon;
     },
 
-    _computeShowOnNeedsAndBlockedFilter(task) {
+    _isFailOrReadyOrInvalid(task) {
       switch (task.status) {
         case 'FAIL':
         case 'READY':
@@ -138,6 +138,12 @@
           return true;
       }
       return false;
+    },
+
+    _computeShowOnNeedsAndBlockedFilter(task) {
+      return this._isFailOrReadyOrInvalid(task) ||
+        (task.sub_tasks && task.sub_tasks.some(t =>
+          this._computeShowOnNeedsAndBlockedFilter(t)));
     },
 
     _compute_counts(task) {
